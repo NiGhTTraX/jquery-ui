49cb41e7784a3608b7d7d9ffd0d203777b7f50fa
Andrei Picus <office.nightcrawler@gmail.com>
2014-01-17 20:51:04 +0200

    Sortable: fix bug with ui.sender on over events
    Do not set ui.sender on over events when item hasn't changed containers, or when
    the item is coming from a draggable.
    
    Fixes #9744: Sortable: incorrect value for ui.sender in sortover events

--- tests/unit/sortable/sortable_events.js
+++ tests/unit/sortable/sortable_events.js
@@ -254,7 +254,38 @@ test( "over", function() {
 	ok( hash.position && ( "top" in hash.position && "left" in hash.position ), "UI hash includes: position" );
 	ok( hash.offset && ( hash.offset.top && hash.offset.left ), "UI hash includes: offset" );
 	ok( hash.item, "UI hash includes: item" );
-	ok( hash.sender, "UI hash includes: sender" );
+	ok( !hash.sender, "UI hash should not include: sender" );
+	equal( overCount, 1, "over fires only once" );
+});
+
+test( "over from draggable", function() {
+	expect( 8 );
+
+	var hash,
+		overCount = 0,
+		item = $( "<div></div>" ).text( "6" ).insertAfter( "#sortable" );
+
+	item.draggable({
+		connectToSortable: "#sortable"
+	});
+	$( "#sortable" ).sortable({
+		over: function( e, ui ) {
+			hash = ui;
+			overCount++;
+		}
+	});
+
+	item.simulate( "drag", {
+		dy: -20
+	});
+
+	ok( hash, "over event triggered" );
+	ok( hash.helper, "UI includes: helper" );
+	ok( hash.placeholder, "UI hash includes: placeholder" );
+	ok( hash.position && ( "top" in hash.position && "left" in hash.position ), "UI hash includes: position" );
+	ok( hash.offset && ( hash.offset.top && hash.offset.left ), "UI hash includes: offset" );
+	ok( hash.item, "UI hash includes: item" );
+	ok( !hash.sender, "UI hash should not include: sender" );
 	equal( overCount, 1, "over fires only once" );
 });
 
